0880ac1a6296e808110cca194b5af2ec1bb59b89
Samuel Cox <samc1253@gmail.com>
2022-11-09 09:29:16 -0700

    Add test for stock method

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -16,4 +16,10 @@ RSpec.describe FoodTruck do
   it 'can check the stock of a specific item' do
     expect(@food_truck.check_stock(@item1)).to eq 0
   end
+
+  it 'can add stock of an item' do
+    @food_truck.stock(@item1, 30)
+
+    expect(@food_truck.check_stock(@item1)).to eq 30
+  end
 end

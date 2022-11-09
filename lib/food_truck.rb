55f8f9d745dc333a8f11954c3d982494f70ee74d
KaraJH <karas.jones@yahoo.com>
2022-11-09 09:32:08 -0700

    Creates food truck check stock method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,12 @@
 class FoodTruck
   attr_reader :name, :inventory
 
-  def initialize(name)
+  def initialize(name, inventory = 0)
     @name = name
     @inventory = {}
+  end
+
+  def check_stock(item_object)
+    stock = @inventory.count
   end
 end

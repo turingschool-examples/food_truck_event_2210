8feb5f583096cc9070cdbe3444e70f1111314782
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-09 10:30:00 -0600

    Pass food truck check stock and stock tests

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,6 +4,18 @@ class FoodTruck
 
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    inventory[item]
+  end
+
+  def stock(item, quantity)
+    if inventory.key?(item)
+      inventory[item] += quantity
+    else 
+      inventory[item] = quantity
+    end
   end
 end

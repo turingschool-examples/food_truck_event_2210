463c963d4c103e589ec289f651abe1bad5681b9b
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-09 11:16:15 -0600

    Write test for adding to existing stock

--- spec/food_truck.rb
+++ spec/food_truck.rb
@@ -34,7 +34,32 @@ RSpec.describe FoodTruck do
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 
     food_truck.stock(item1, 30)
-
+    # require 'pry' ; binding.pry
     expect(food_truck.check_stock(item1)).to eq(30)
+  end
+
+  it 'can see if it has an item as a key' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    expect(food_truck.key_match(item1)).to be(false)
+
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.key_match(item1)).to be(true)
+  end
+
+  it 'can add to a stock' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    food_truck.stock(item1, 30)
+    expect(food_truck.check_stock(item1)).to eq(30)
+
+     # require 'pry' ; binding.pry
+    food_truck.stock(item1, 25)
+
+    expect(food_truck.check_stock(item1)).to eq(55)
+
   end
 end

3c34f47ac72181846cdc8c0e20dc9845ca9be341
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-09 08:09:01 -0800

    Initial commit

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,29 @@
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+    it 'exists' do
+        
+        
+        
+        
+        
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+        
+        food_truck.name
+        
+        food_truck.inventory
+        #=> {}
+        food_truck.check_stock(item1)
+        #=> 0
+        food_truck.stock(item1, 30)
+        food_truck.inventory
+        #=> {#<Item:0x007f9c56740d48...> => 30}
+        food_truck.check_stock(item1)
+        #=> 30
+        food_truck.stock(item1, 25)
+        food_truck.check_stock(item1)
+        #=> 55
+        food_truck.stock(item2, 12)
+        food_truck.inventory
+        #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}    end
+end

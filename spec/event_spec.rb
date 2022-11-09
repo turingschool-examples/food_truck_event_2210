adff6426492021b08934836ccabacb359c85517b
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-09 11:07:15 -0700

    test: Add tests for event class it3

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe Event do
 
   let(:food_truck1) { FoodTruck.new('Rocky Mountain Pies')}
   let(:food_truck2) { FoodTruck.new('Ba-Nom-a-Nom')}
+  let(:food_truck3) { FoodTruck.new('Buns-a-lot')}
+  let(:food_truck4) { FoodTruck.new('Tacos')}
 
   let(:item1) { Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' }) }
   let(:item2) { Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' }) }
   let(:item3) { Item.new({ name: 'Peach-Raspberry Nice Cream', price: '$5.30' }) }
-  let(:item4) { Item.new({ name: 'Banana Nice cream', price: '$4.25' }) }
+  let(:item4) { Item.new({ name: 'Banana Nice Cream', price: '$4.25' }) }
 
   describe '#initialize' do
     it 'exists' do
@@ -61,4 +63,65 @@ RSpec.describe Event do
     end
   end
 
+  describe '#overstocked_items' do
+    it 'returns a list of items in > 1 truck and count > 50' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      event.add_food_truck(food_truck4)
+
+      food_truck1.stock(item1, 20)
+      food_truck1.stock(item2, 20)
+  
+      expect(event.overstocked_items).to eq([])
+
+      food_truck1.stock(item1, 45)
+      food_truck2.stock(item1, 20)
+
+      expect(event.overstocked_items).to eq([item1])
+
+      food_truck1.stock(item2, 45)
+      food_truck2.stock(item2, 20)
+
+      expect(event.overstocked_items).to eq([item1, item2])
+    end
+  end
+
+  describe '#available_item_names' do
+    it 'lists all names of available items at event alphabetically' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.available_item_names).to eq([])
+
+      food_truck1.stock(item1, 20)
+      food_truck1.stock(item2, 20)
+      expect(event.available_item_names).to eq(['Apple Pie (Slice)', 'Peace Pie (Slice)'])
+
+      food_truck2.stock(item3, 45)
+      food_truck3.stock(item3, 45)
+      food_truck4.stock(item4, 45)
+
+      expect(event.available_item_names).to eq(['Apple Pie (Slice)', 'Banana Nice Cream' 'Peach-Raspberry Nice Cream', 'Peach Pie (Slice)'])
+    end
+  end
+
+  describe '#total_event_inventory' do
+    it 'returns a hash of total event inventory' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      event.add_food_truck(food_truck4)
+
+      food_truck1.stock(item1, 20)
+      food_truck1.stock(item2, 20)
+      food_truck2.stock(item3, 45)
+      food_truck3.stock(item4, 20)
+      food_truck3.stock(item2, 45)
+      food_truck4.stock(item2, 20)
+
+      expect(event.total_event_inventory).to eq({})
+    end
+  end
 end

62201ff1e03b312dac73d00e29ab2aa44f50b1a9
Joe King <j.king92@gmail.com>
2022-11-09 09:34:28 -0700

    test: #initialize and that name can be accessed

---
+++ spec/event_spec.rb
@@ -0,0 +1,12 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  let(:event) { Event.new('Catalina Wine Mixer')}
+
+  it 'exists and has a name' do
+    expect(event).to be_a Event
+    expect(event.name).to eq('Catalina Wine Mixer')
+  end
+end

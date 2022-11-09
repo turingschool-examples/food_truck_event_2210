3d58f7301bc3c4bc78bbc541a7ec14a6c68c085c
Aether Kerstens <philippekerstens@gmail.com>
2022-11-09 08:07:19 -0800

    Create item name  unit test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Item do
   it '#exists' do
     expect(item1).to be_an_instance_of(Item)
   end
+
+
+  it 'has name' do
+    expect(item1.name).to eq('Peach Pie(Slice)')
+  end
+
+  it 'has price' do
+    expect(item1.price).to eq("$3.75")
+  end
 end

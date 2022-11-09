fe538157d56c41913f1faa9a2a41bec7ed751616
Ryan Canton <ryancanton999@gmail.com>
2022-11-09 10:14:45 -0700

    Test: Add test for all_items method for class Event

--- lib/event.rb
+++ lib/event.rb
@@ -19,4 +19,17 @@ class Event
   def food_trucks_that_sell(item)
     food_trucks.find_all { |food_truck| food_truck.check_stock(item) > 0}
   end
+
+  def overstocked_items
+    items = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity|
+        total_quantity = food_trucks_that_sell(item).map { |food_truck| food_truck.check_stock(item) }.sum
+        if food_trucks_that_sell(item).count > 1 && total_quantity > 50
+          items.push(item)
+        end
+      end
+    end
+    items.uniq
+  end
 end

d0183a23f36169d3ec82d585968af4f3468fc046
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-09 09:18:21 -0700

    feat: adds name and inventory

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -6,8 +6,10 @@ require './lib/food_truck'
 describe FoodTruck do
   describe '#initialize' do
     it 'exists and has attributes' do
-      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
       expect(food_truck).to be_instance_of(FoodTruck)
+      expect(food_truck.name).to eq('Rocky Mountain Pies')
+      expect(food_truck.inventory).to eq({})
     end
   end
 end

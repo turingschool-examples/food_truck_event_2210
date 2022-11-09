79375526c8956657c3a2f31c30c0c6ad456f1ed7
Alex Pitzel <pitzelalex@gmail.com>
2022-11-09 11:07:54 -0500

    feat: Create Item class and tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/item'
+
+describe Item do
+  describe '#initialize' do
+    it 'instantiates correctly' do
+      item1 = Item.new({ name: 'Peach Pie (Slice)', price: "$3.75" })
+      item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
+
+      expect(item1).to be_a Item
+      expect(item2).to be_a Item
+      expect(item1.name).to eq('Peach Pie (Slice)')
+      expect(item2.name).to eq('Apple Pie (Slice)')
+      expect(item1.price).to eq('$3.75')
+      expect(item2.price).to eq('$2.50')
+    end
+  end
+end

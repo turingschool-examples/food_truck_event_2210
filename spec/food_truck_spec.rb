f3e8a2c47e6c97a560fd468c34717772917db60d
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-09 09:28:59 -0700

    test: stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -22,4 +22,19 @@ describe FoodTruck do
       expect(food_truck.check_stock(item1)).to eq(0)
     end
   end
+
+  describe '#stock' do
+    it 'adds items to the inventory collection' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      food_truck.stock(item1, 25)
+
+      expect(food_truck.check_stock(item1)).to eq(25)
+
+      food_truck.stock(item2, 12)
+
+      expect(food_truck.check_stock(item2)).to eq(12)
+    end
+  end
 end

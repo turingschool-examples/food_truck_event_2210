87450b87b2ef659f7f1070d9d08359ba51f9cc90
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-09 10:10:51 -0700

    Test: event exists and has attributes

---
+++ spec/event_spec.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+require 'pry'
+
+RSpec.describe Event do
+    it 'event exists and has attributes' do
+        event = Event.new("South Pearl Street Farmers Market")
+
+        expect(event).to be_instance_of(Event)
+        expect(event.name).to eq ("South Pearl Street Farmers Market")
+        expect(event.food_trucks).to eq([])
+    end
+end

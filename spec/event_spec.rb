d47a91e33d4fdb737d4cf33e5e17d9955e2c347c
Thomas Casady <tscasady@gmail.com>
2022-11-09 11:09:55 -0600

    test: Add overstocked_items, all_items_by_name, and item_index tests.

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -43,4 +43,45 @@ RSpec.describe Event do
       expect(event.food_trucks_that_sell(item1)).to eq [food_truck1, food_truck2]
     end
   end
+
+  describe '#overstocked_items' do
+    it 'returns an array of items who are overstocked' do
+      food_truck1.stock(item1, 55)
+      food_truck1.stock(item2, 33)
+      food_truck2.stock(item1, 10)
+      food_truck2.stock(item2, 22)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      expect(event.overstocked_items).to eq([item1, item2])
+    end
+  end
+
+  describe '#all_items_by_name' do
+    it 'returns an alphabetized list of all item names sold at the event' do
+      food_truck1.stock(item1, 55)
+      food_truck1.stock(item2, 33)
+      food_truck2.stock(item1, 10)
+      food_truck2.stock(item2, 22)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      expect(event.all_items_by_name).to eq [item2, item1]
+    end
+  end
+
+  describe '#item_index' do
+    it 'returns a hash whose keys are items and values are total inventory and food_trucks that sell that item' do
+      food_truck1.stock(item1, 55)
+      food_truck1.stock(item2, 33)
+      food_truck2.stock(item1, 10)
+      food_truck2.stock(item2, 22)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      expect(event.item_index).to eq(
+        {
+          item1: { total_inventory: 65, sold_by: [food_truck1, food_truck2] },
+          item2: { total_inventory: 55, sold_by: [food_truck1, food_truck2] }
+        }
+      )
+    end
+  end
 end

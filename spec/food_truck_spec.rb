211c18d8ba2215967dd47aa60ed853ca9d43cbd2
KaraJH <karas.jones@yahoo.com>
2022-11-09 09:36:20 -0700

    Tests for the stock method

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -27,4 +27,16 @@ RSpec.describe FoodTruck do
       expect(food_truck.check_stock(item1)).to eq(0)
     end
   end
+
+  describe '#stock' do
+    it 'stocks a number of items in the inventory' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      food_truck.stock(item1, 30)
+      expect(food_truck.check_stock(item1)).to eq(30)
+      expect(food_truck.inventory.length).to eq(30)
+    end
+  end
 end

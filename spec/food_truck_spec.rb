02279c522beaea923b2ab344c2a857ce5503a6f1
Samuel Cox <samc1253@gmail.com>
2022-11-09 09:32:17 -0700

    Add test to make testing of stock method more robust

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -17,9 +17,19 @@ RSpec.describe FoodTruck do
     expect(@food_truck.check_stock(@item1)).to eq 0
   end
 
-  it 'can add stock of an item' do
+  it 'can add stock of a new item' do
     @food_truck.stock(@item1, 30)
 
     expect(@food_truck.check_stock(@item1)).to eq 30
+  end
+
+  it 'can add stock to an existing item' do
+    @food_truck.stock(@item1, 30)
+
+    expect(@food_truck.check_stock(@item1)).to eq 30
+
+    @food_truck.stock(@item1, 25)
+
+    expect(@food_truck.check_stock(@item1)).to eq 55
   end
 end

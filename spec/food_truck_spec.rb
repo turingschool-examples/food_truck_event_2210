1a0de68787c90e5de0bd37d1d3da3d89f4daad64
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-09 10:20:00 -0700

    test: inventory value

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -44,14 +44,33 @@ describe FoodTruck do
     end
   end
 
-  describe '#has_item' do
+  describe '#item?' do
     it 'returns whether the truck stocks in inventory specified item' do
       food_truck1 = FoodTruck.new('Tacos')
       item1 = Item.new(name: 'Steak Taco', price: '$1.25')
 
       food_truck1.stock(item1, 100)
 
-      expect(food_truck1.has_item(item1)).to eq(true)
+      expect(food_truck1.item?(item1)).to eq(true)
+    end
+  end
+
+  describe '#inventory_value' do
+    it 'calculates all current inventory value' do
+      food_truck = FoodTruck.new('Rocky Mountain Pies')
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      food_truck.stock(item1, 25)
+
+      expect(food_truck.check_stock(item1)).to eq(25)
+
+      food_truck.stock(item2, 1)
+
+      expect(food_truck.check_stock(item2)).to eq(12)
+
+      food_truck.stock(item1, 1)
+
+      expect(food_truck1.inventory_value).to eq(6.25)
     end
   end
 end

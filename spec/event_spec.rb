73b5ed3ce93935ae3f7a19620304109634f5ec11
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-09 10:32:52 -0700

    test: oversold_items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rspec'
+require './lib/food_truck'
+require './lib/item'
 require './lib/event'
 
 describe Event do
@@ -67,6 +69,30 @@ describe Event do
       expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
       expect(event.food_trucks_that_sell(item3)).to eq([food_truck2])
       expect(event.food_trucks_that_sell('')).to eq([])
+    end
+  end
+
+  describe '#oversold_items' do
+    it 'return a list of overstocked Items for an Event' do
+      event = Event.new('South Pearl Street Farmers Market')
+      food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+      item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+      food_truck2.stock(item4, 55)
+      food_truck2.stock(item3, 25)
+      food_truck3 = FoodTruck.new('Palisade Peach Shack')
+      food_truck3.stock(item1, 65)
+
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.oversold_items).to eq(')'
     end
   end
 end

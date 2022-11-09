5a659501f0a089e5064224d48df86001a685bf3f
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-09 11:48:50 -0600

    Add event overstocked items and total item quantity tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -86,4 +86,64 @@ describe Event do
       expect(event.food_trucks_that_sell(item4)).to eq [food_truck2]
     end
   end
+
+  describe '#total_item_quantity' do
+    it 'returns the total quantity of an item across all food trucks' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      food_truck1.stock(item1, 35)    
+      food_truck1.stock(item2, 7)    
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")    
+      food_truck2.stock(item4, 51)    
+      food_truck2.stock(item3, 25)
+      food_truck2.stock(item2, 25)
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")    
+      food_truck3.stock(item1, 65)  
+
+      event.add_food_truck(food_truck1)    
+      event.add_food_truck(food_truck2)    
+      event.add_food_truck(food_truck3)
+
+      expect(event.total_item_quantity(item1)).to eq 100
+      expect(event.total_item_quantity(item2)).to eq 32
+      expect(event.total_item_quantity(item4)).to eq 51
+    end
+  end
+
+  describe '#overstocked_items' do
+    it 'returns a list of items sold by more than 1 food truck with a quantity over 50' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      food_truck1.stock(item1, 35)    
+      food_truck1.stock(item2, 7)    
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")    
+      food_truck2.stock(item4, 51)    
+      food_truck2.stock(item3, 25)
+      food_truck2.stock(item2, 25)
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")    
+      food_truck3.stock(item1, 65)  
+
+      event.add_food_truck(food_truck1)    
+      event.add_food_truck(food_truck2)    
+      event.add_food_truck(food_truck3)
+
+      expect(event.food_trucks_that_sell(item1)).to eq [food_truck1, food_truck3]
+      expect(event.food_trucks_that_sell(item2)).to eq [food_truck1, food_truck2]
+      expect(event.food_trucks_that_sell(item4)).to eq [food_truck2]
+
+      expect(event.total_item_quantity(item1)).to eq 100
+      expect(event.total_item_quantity(item2)).to eq 32
+      expect(event.total_item_quantity(item4)).to eq 51
+
+      expect(event.overstocked_items).to eq [item1]
+    end
+  end
 end

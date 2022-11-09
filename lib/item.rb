c5a4b860eacbaec7e375ae2d054fc75419808a9c
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-09 09:11:41 -0700

    feat: adds name and price

--- lib/item.rb
+++ lib/item.rb
@@ -2,7 +2,14 @@
 
 # This is the Item Class
 class Item
-  def initialize()
-    ''
+
+  attr_reader :name
+  def initialize(args)
+    @name  = args[:name]
+    @price = args[:price]
+  end
+
+  def price
+    @price.delete('$').to_f
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,9 +5,11 @@ require './lib/item'
 
 describe Item do
   describe '#initialize' do
-    it 'exists' do
-      item = Item.new
+    it 'exists and has attributes' do
+      item = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
       expect(item).to be_instance_of(Item)
+      expect(item.name).to eq('Peach Pie (Slice)')
+      expect(item.price).to eq(3.75)
     end
   end
 end

e9f99999622a5e4ee5f7fd7b96424be20bcbf375
Braxton Eddings <beddings81@gmail.com>
2022-11-09 10:27:55 -0600

    Complete I1

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,25 @@
 class FoodTruck
+  attr_reader :name,
+              :inventory
 
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new
+  end
+
+  def check_stock(item)
+    if @inventory[item] == nil
+       0
+    else
+      @inventory[item]
+    end
+  end
+
+  def stock(item, quantity)
+    if @inventory[item].nil?
+      @inventory[item] = quantity
+    else
+      @inventory[item] += quantity
+    end
+  end
 end

6a3ec9c83d61dfb7b18f7ac6cf29807c87c26150
KaraJH <karas.jones@yahoo.com>
2022-11-09 10:50:18 -0700

    Notes to come back to stock method, stuck on transforming values

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -29,14 +29,17 @@ RSpec.describe FoodTruck do
   end
 
   describe '#stock' do
-    it 'stocks a number of items in the inventory' do
+    xit 'stocks a number of items in the inventory' do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
       item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
       item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
       food_truck.stock(item1, 30)
       expect(food_truck.check_stock(item1)).to eq(30)
-      expect(food_truck.inventory.length).to eq(30)
+
+      food_truck.stock(item1, 25)
+      expect(food_truck.check_stock(item1)).to eq(55)
+      #figure out how to transform values 
     end
   end
 end

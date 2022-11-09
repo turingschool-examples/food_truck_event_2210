7e82d18cb90fa0892a8088f08629d9384495cc13
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-09 09:17:11 -0700

    Test: food truck exists and has attributes

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,11 @@
+require './lib/food_truck'
+require 'pry'
+
+RSpec.describe FoodTruck do
+  it 'food_truck exists and has attributes' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck).to be_instance_of(FoodTruck)
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+end

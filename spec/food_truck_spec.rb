d20008526b996b39bca61bde7122bcc6c09daa34
Joe King <j.king92@gmail.com>
2022-11-09 09:14:28 -0700

    test: That the food truck has a name

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,8 +3,14 @@ require './lib/food_truck'
 
 RSpec.describe FoodTruck do
   it 'exists' do
-    ft = FoodTruck.new("Rock Springs Cafe")
+    ft = FoodTruck.new('Rock Springs Cafe')
 
     expect(ft).to be_a FoodTruck
+  end
+
+  it 'has a name' do
+    ft = FoodTruck.new('Rock Springs Cafe')
+
+    expect(ft.name).to eq('Rock Springs Cafe')
   end
 end

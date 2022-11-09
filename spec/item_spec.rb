7ac931af005d88e5ede011305d98482400b67895
Kelsie Porter <krporter22@gmail.com>
2022-11-09 09:15:45 -0700

    Create Item class and associated tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+
+RSpec.describe Item do 
+  it 'exists and has attributes' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"}) 
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    expect(item1.name).to eq('Peach Pie (Slice)')
+    expect(item2.price).to eq(2.50)
+  end
+end

01f2baa8586bd64c8d9f1f3a40634934ac92d2fd
Samuel Cox <samc1253@gmail.com>
2022-11-09 09:43:39 -0700

    Update testing framework for event to reflect interaction pattern

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,8 +6,17 @@ RSpec.describe Event do
   before :each do
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    @food_truck = FoodTruck.new('Rocky Mountain Pies')
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
     @event = Event.new('South Pearl Street Farmers Market')
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
   end
 
   it 'exists and has attributes' do

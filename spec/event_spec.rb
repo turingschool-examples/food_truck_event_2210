f227c7718efe02e6d996e8c362213caeb323a99a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-09 11:59:46 -0600

    Add event all event items test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -114,6 +114,31 @@ describe Event do
     end
   end
 
+  describe '#all_event_items' do
+    it 'returns a list of all items sold at the event' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      food_truck1.stock(item1, 35)    
+      food_truck1.stock(item2, 7)    
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")    
+      food_truck2.stock(item4, 51)    
+      food_truck2.stock(item3, 25)
+      food_truck2.stock(item2, 25)
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")    
+      food_truck3.stock(item1, 65)  
+
+      event.add_food_truck(food_truck1)    
+      event.add_food_truck(food_truck2)    
+      event.add_food_truck(food_truck3)
+
+      expect(event.all_event_items).to eq [item1, item2, item3, item4]
+    end
+  end
+ 
   describe '#overstocked_items' do
     it 'returns a list of items sold by more than 1 food truck with a quantity over 50' do
       event = Event.new("South Pearl Street Farmers Market")

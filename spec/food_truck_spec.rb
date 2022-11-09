aed1181a2c54bbb93afc770ed25859da0c54213f
Joe King <j.king92@gmail.com>
2022-11-09 09:17:23 -0700

    test: #inventory starts as an empty hash

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,15 +2,18 @@ require './lib/item'
 require './lib/food_truck'
 
 RSpec.describe FoodTruck do
+  let(:ft) {FoodTruck.new('Rock Springs Cafe')}
   it 'exists' do
-    ft = FoodTruck.new('Rock Springs Cafe')
-
     expect(ft).to be_a FoodTruck
   end
 
   it 'has a name' do
-    ft = FoodTruck.new('Rock Springs Cafe')
-
     expect(ft.name).to eq('Rock Springs Cafe')
+  end
+
+  describe '#inventory' do
+    it 'starts as an empty hash' do
+      expect(ft.inventory).to eq({})
+    end
   end
 end

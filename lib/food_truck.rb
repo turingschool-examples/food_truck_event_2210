de89d1413895e1824cb8d1b0984214001485f7ea
Aether Kerstens <philippekerstens@gmail.com>
2022-11-09 09:58:39 -0800

    Finish food_truck

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,19 @@
 class FoodTruck
+  attr_reader :name, :inventory
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
 
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    if @inventory[item] == nil
+      @inventory[item] = quantity
+    else
+      @inventory[item] += quantity
+    end
+  end
 end

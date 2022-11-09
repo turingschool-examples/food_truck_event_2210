745d4b71bb447a0d81bd8b75011f53b60aa1477c
Thomas Casady <tscasady@gmail.com>
2022-11-09 10:45:49 -0600

    test: Add event class tests.

---
+++ spec/event_spec.rb
@@ -0,0 +1,44 @@
+require './lib/event'
+require './lib/food_truck'
+require './lib/item'
+
+RSpec.describe Event do
+  let(:event) { Event.new('South Pearl Street Farmers Market') }
+  let(:food_truck1) { FoodTruck.new("Rocky Mountain Pies") }
+  let(:food_truck2) { FoodTruck.new('Ba-Nom-a-Nom') }
+  let(:item1) { Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' }) }
+  let(:item2) { Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' }) }
+  let(:item3) { Item.new({ name: 'Peach-Raspberry Nice Cream', price: '$5.30'}) }
+  let(:item4) { Item.new({ name: 'Banana Nice Cream', price: '$4.25'}) }
+  describe '#initialize' do
+    it 'exists and has readable attributes' do
+      expect(event).to be_a Event
+      expect(event.name).to eq 'South Pearl Street Farmers Market'
+      expect(event.food_trucks).to eq []
+    end
+  end
+
+  describe '#add_food_truck' do
+    it 'adds a food_truck to food_trucks array' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      expect(event.food_trucks).to eq [food_truck1, food_truck2]
+    end
+  end
+
+  describe '#food_truck_names' do
+    it 'returns an array of food_truck names associated with the event' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      expect(event.food_truck_names).to eq ['Rocky Mountain Pies', 'Ba-Nom-a-Nom']
+    end
+  end
+
+  describe '#food_trucks_that_sell' do
+    it 'returns an array of food_trucks that sell a given item' do
+      food_truck1.stock(item1, 1)
+      food_truck2.stock(item1, 2)
+      expect(event.food_trucks_that_sell(item1)).to eq [food_truck1, food_truck2]
+    end
+  end
+end

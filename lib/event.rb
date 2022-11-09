b02ddb3a2bc8c65e124c65fb272850d647eaa6be
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-09 10:14:13 -0700

    Feat: add food trucks that sell method for class Event

--- lib/event.rb
+++ lib/event.rb
@@ -13,4 +13,8 @@ class Event
   def food_truck_names
     @food_trucks.map {|truck| truck.name}
   end 
+
+  def food_trucks_that_sell(item)
+    food_trucks.find_all {|truck| truck.inventory.include?(item)}
+  end 
 end

1bbb4b7453ee9e39d39bc93cc2478a504b63f185
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-09 09:19:37 -0700

    Feat: add initialize method for class FoodTruck

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe FoodTruck do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
 
       expect(food_truck).to be_a(FoodTruck)
-      expect(food_truck.name).to eq("Rock Mountain Pies")
+      expect(food_truck.name).to eq("Rocky Mountain Pies")
       expect(food_truck.inventory).to eq({})
     end 
   end 

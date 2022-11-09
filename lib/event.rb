bd65b83080f4572d775bc396f86cb620cb421db7
Alastair Lloyd <dex8056@aol.com>
2022-11-09 10:29:38 -0700

    Add and pass test for food truck names

--- lib/event.rb
+++ lib/event.rb
@@ -6,4 +6,14 @@ class Event
     @name = name
     @food_trucks = []
   end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+  end
+
+  def food_truck_names
+    @food_trucks.map do |food_truck|
+      food_truck.name
+    end
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -22,4 +22,18 @@ RSpec.describe Event do
       expect(event.food_trucks).to eq [food_truck1, food_truck2]
     end
   end
+
+  describe '#food_truck_names' do
+    it 'returns an array of the food truck names' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      expect(event.food_truck_names).to eq ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]   
+    end
+  end
 end

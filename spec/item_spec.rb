d5bf3082d6dd0a214f1835521a8af6089e217436
Ryan Canton <ryancanton999@gmail.com>
2022-11-09 09:06:07 -0700

    Test: Add test for initialize method for class Item

---
+++ spec/item_spec.rb
@@ -0,0 +1,15 @@
+require './lib/item'
+
+
+RSpec.describe Item do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+      expect(item1).to be_a(Item)
+      expect(item1.name).to eq('Peach Pie (Slice)')
+      expect(item1.price).to eq(3.75)
+    end
+  end
+
+end

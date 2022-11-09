2dedc571481a07347a9bea9fb1b5277adb84c9c3
Drew Layton <dlayton66@gmail.com>
2022-11-09 09:44:11 -0700

    feat/test: Create Event class

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,16 @@
-class Event
+require './lib/item'
+require './lib/food_truck'
 
+class Event
+  attr_reader :name,
+              :food_trucks
+
+ def initialize(name)
+  @name = name
+  @food_trucks = []
+ end
+
+ def add_food_truck
+
+ end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,5 +1,17 @@
 require './lib/event'
 
 describe Event do
+  let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+  let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+  let(:item3) {Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})}
+  let(:item4) {Item.new({name: "Banana Nice Cream", price: "$4.25"})}
+  let(:food_truck1) {FoodTruck.new("Rocky Mountain Pies")}
+  let(:food_truck2) {FoodTruck.new("Ba-Nom-a-Nom")}
+  let(:food_truck3) {FoodTruck.new("Palisade Peach Shack")}
+  let(:event) {Event.new("South Pearl Street Farmers Market")}
 
+  it 'exists and has attributes' do
+    expect(event.name).to eq("South Pearl Street Farmers Market")
+    expect(event.food_trucks).to eq([])
+  end
 end

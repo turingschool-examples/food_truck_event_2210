e0b74e37a7f7471502f49b4059bddd27965487a4
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-09 09:25:30 -0700

    feat: Add item class and methods

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,12 @@
 class Item
+  attr_reader :name, :price
 
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = clean_price(attributes[:price])
+  end
+
+  def clean_price(price)
+    price[1..-1].to_f.round(2)
+  end
 end

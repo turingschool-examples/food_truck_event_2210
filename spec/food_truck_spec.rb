2467ef21460ae1e3d01b19cb33a2aa9865e58e17
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-09 10:33:27 -0600

    Add more rigorous tests to the food truck stock tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -31,10 +31,34 @@ describe FoodTruck do
 
       expect(food_truck.inventory).to eq({item1 => 30})
       expect(food_truck.check_stock(item1)).to eq 30
+    end
+
+    it 'adds to an items quantity if the item is already in stock' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.inventory).to eq({item1 => 30})
+      expect(food_truck.check_stock(item1)).to eq 30
 
       food_truck.stock(item1, 25)
 
       expect(food_truck.check_stock(item1)).to eq 55
+    end
+
+    it 'can add different items to the same inventory' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.inventory).to eq({item1 => 30})
+      
+      food_truck.stock(item2, 12)
+
+      expect(food_truck.inventory).to eq({item1 => 30, item2 => 12})
     end
   end
 end

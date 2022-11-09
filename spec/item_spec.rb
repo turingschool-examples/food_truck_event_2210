d66f6dc14d2973f93e74ad5a0bff6b19fd561311
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-09 09:08:05 -0700

    Test: describe Item

---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+require pry
+
+RSpec.describe Item do
+  it 'item exists and has attributes' do
+    item = item.new({name: 'Peach Pie (Slice)', price: "$3:75"})
+
+    expect(item).to be_instance_of(Item)
+    expect(item.name).to eq("Peach Pie (Slice)")
+  end
+end

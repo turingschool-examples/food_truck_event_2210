324f237e6d5632c1b41cece3408f31c0049ae72e
Ryan Canton <ryancanton999@gmail.com>
2022-11-09 10:04:25 -0700

    Test: Add test for overstocked_items for class Event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -64,4 +64,28 @@ RSpec.describe Event do
       expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
     end
   end
+
+  describe '#overstocked_items' do
+    it 'returns a list of items sold by more than 1 food truck AND the total quantity is greater than 50' do
+      event = Event.new("South Pearl Street Farmers Market")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      food_truck3.stock(item4, 12)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.overstocked_items).to eq([item1, item4])
+    end
+  end
 end

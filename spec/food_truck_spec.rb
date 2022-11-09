0d7ccc2637c613a5fb3a4e6d54eb0d54a9f8ac03
Samuel Cox <samc1253@gmail.com>
2022-11-09 09:56:38 -0700

    Add test for returning potential revenue for a specific truck

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe FoodTruck do
   before :each do
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @food_truck = FoodTruck.new('Rocky Mountain Pies')
   end
 
@@ -47,5 +49,19 @@ RSpec.describe FoodTruck do
 
     expect(@food_truck.check_stock(@item2)).to eq 12
     expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+  end
+
+  it 'can return the potential revenue for a specific truck' do
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    @food_truck.stock(@item1, 35)
+    @food_truck.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+
+    expect(@food_truck.potential_revenue).to eq 148.75
+    expect(@food_truck2.potential_revenue).to eq 345
+    expect(@food_truck3.potential_revenue).to eq 243.75
   end
 end

aafaba61582cf4b4ee84a46638ef1e0247919082
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-09 11:30:20 -0700

    test: total inventory

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -145,7 +145,7 @@ describe Event do
   end
 
   describe '#list_all_items_sold_names' do
-    it 'lists all of the items sold at event' do
+    it 'lists all of the item\'s names sold at event' do
       event = Event.new('South Pearl Street Farmers Market')
       food_truck1 = FoodTruck.new('Rocky Mountain Pies')
       item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
@@ -167,4 +167,27 @@ describe Event do
       expect(event.list_all_items_sold_names).to eq(['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream'])
     end
   end
-end
+
+  describe '#total_inventory' do
+    it 'lists all of the items sold at event and their corresponding stats' do
+      event = Event.new('South Pearl Street Farmers Market')
+      food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+      item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+      food_truck2.stock(item4, 55)
+      food_truck2.stock(item3, 25)
+      food_truck3 = FoodTruck.new('Palisade Peach Shack')
+      food_truck3.stock(item1, 65)
+
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.total_inventory).to eq(['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream'])
+    end
+  end

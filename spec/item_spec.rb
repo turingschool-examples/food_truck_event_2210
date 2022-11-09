4d00a846838ecf11e348414d53921d20ce6e3fe8
Aether Kerstens <philippekerstens@gmail.com>
2022-11-09 08:12:02 -0800

    Pass item tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,16 +1,18 @@
 require './lib/item'
 
 RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+  end
   it '#exists' do
-    expect(item1).to be_an_instance_of(Item)
+    expect(@item1).to be_an_instance_of(Item)
   end
 
-
   it 'has name' do
-    expect(item1.name).to eq('Peach Pie(Slice)')
+    expect(@item1.name).to eq('Peach Pie (Slice)')
   end
 
   it 'has price' do
-    expect(item1.price).to eq("$3.75")
+    expect(@item1.price).to eq("$3.75")
   end
 end

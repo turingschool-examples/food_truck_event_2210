da154a8e83e7dc55df29b9e539296b0db2c7d6f8
Alastair Lloyd <dex8056@aol.com>
2022-11-09 10:44:51 -0700

    Add and pass food trucks that sell test

--- lib/event.rb
+++ lib/event.rb
@@ -16,4 +16,10 @@ class Event
       food_truck.name
     end
   end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.select do |food_truck|
+      food_truck.check_stock(item) > 0
+    end
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,3 +1,5 @@
+require './lib/item'
+require './lib/food_truck'
 require './lib/event'
 
 RSpec.describe Event do
@@ -29,11 +31,27 @@ RSpec.describe Event do
       food_truck1 = FoodTruck.new("Rocky Mountain Pies")
       food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
       food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      
 
       event.add_food_truck(food_truck1)
       event.add_food_truck(food_truck2)
       event.add_food_truck(food_truck3)
       expect(event.food_truck_names).to eq ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]   
+    end
+  end
+
+  describe '#food_trucks_that_sell' do
+    it 'returns an array of food trucks that sell the item' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      event.add_food_truck(food_truck1)
+            
+      food_truck1.stock(item1, 30)
+      expect(event.food_trucks_that_sell(item1)).to eq [food_truck1]
+      expect(event.food_trucks_that_sell(item2)).to eq []
     end
   end
 end

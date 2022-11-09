3cf7169a7f0ae16bdcb5f4ac27d44582d9e5a717
Ryan Canton <ryancanton999@gmail.com>
2022-11-09 09:12:09 -0700

    Test: Add test for initialize method of class FoodTruck

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,2 +1,15 @@
 require './lib/item'
 require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      expect(food_truck).to be_a(FoodTruck)
+      expect(food_truck.name).to eq("Rocky Mountain Pies")
+      expect(food_truck.inventory).to eq({})
+    end
+  end
+
+end

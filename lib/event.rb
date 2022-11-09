4d294149f5c860da0563bb4f397a0da28b6bbaea
KaraJH <karas.jones@yahoo.com>
2022-11-09 11:02:34 -0700

    Creates add food truck method

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,12 @@
 class Event
+  attr_reader :name, :food_trucks
 
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(food_truck_object)
+    @food_trucks << food_truck_object
+  end
 end

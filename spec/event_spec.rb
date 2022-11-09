9f162115607c271ab884b1df1c7694e7f495d976
Drew Layton <dlayton66@gmail.com>
2022-11-09 09:55:10 -0700

    feat/test: Create #food_truck_names for Event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -30,4 +30,14 @@ describe Event do
       expect(event.food_trucks).to eq([food_truck1,food_truck2,food_truck3])
     end
   end
+
+  describe '#food_truck_names' do
+    it 'returns an array of FoodTruck names' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
 end

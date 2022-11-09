fd6a0497f0c22c071025a70ecbacbc950f858935
Kelsie Porter <krporter22@gmail.com>
2022-11-09 09:53:44 -0700

    feat: Create #check_stock, #stock, and associated tests

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,7 +4,15 @@ class FoodTruck
 
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
   end
 
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,8 +4,40 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do 
   it 'exists and has attributes' do 
     food_truck = FoodTruck.new("Rocky Mountain Pies")
-    
+
+    expect(food_truck).to be_a(FoodTruck)
     expect(food_truck.name).to eq("Rocky Mountain Pies")
     expect(food_truck.inventory).to eq({})
   end
+
+  it 'can check the stock of items in inventory' do 
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.inventory).to eq({})
+    expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+  it 'can add items to its inventory and check its stock' do 
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    food_truck.stock(item1, 30)
+    
+    expect(food_truck.inventory).to eq({item1 => 30})
+    expect(food_truck.check_stock(item1)).to eq(30)
+
+    food_truck.stock(item1, 25)
+
+    expect(food_truck.inventory).to eq({item1 => 55})
+    expect(food_truck.check_stock(item1)).to eq(55)
+
+    food_truck.stock(item2, 12)
+
+    expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
+  end
+  
+
 end

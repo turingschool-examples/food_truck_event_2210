def73bb93f8b61d807ae66d2d95a3871204baa66
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-09 10:34:09 -0600

    Will have test written for the stock method

---
+++ spec/food_truck.rb
@@ -0,0 +1,40 @@
+require './lib/food_truck'
+require './lib/item'
+
+
+RSpec.describe FoodTruck do
+
+  it 'exist' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck).to be_a(FoodTruck)
+  end
+
+  it 'has a name' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+
+  it 'has an inventory' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.inventory).to eq({})
+  end
+
+  it 'can check the stock' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+  it 'can stock items' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.check_stock(item1)).to eq(30)
+  end
+end

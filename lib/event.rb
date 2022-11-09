50efbebaa323a6631f39dc70a9885d73887f5cf6
Aether Kerstens <philippekerstens@gmail.com>
2022-11-09 10:14:50 -0800

    Create and pass event class

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,24 @@
 class Event
+  attr_reader :name, :food_trucks
 
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+  end
+
+  def food_truck_names
+    @food_trucks.map do |food_truck|
+      food_truck.name
+    end
+  end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.find_all do |food_truck|
+      food_truck.inventory[item] > 0
+    end
+  end
 end

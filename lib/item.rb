d88dbf34ef1f30e945eca6dff511cb13629c94ac
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-09 08:17:41 -0800

    Create Item

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,14 @@
-class Item
+require 'bigdecimal'
 
+class Item
+  attr_reader :name, :price
+
+  def initialize(info)
+    @name = info[:name]
+    @price = to_price(info[:price])
+  end
+
+  def to_price(string)
+    BigDecimal(string.delete('$'))
+  end
 end

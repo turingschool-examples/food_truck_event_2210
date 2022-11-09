02a9912c88b5f252ddce57336e2ccf19d84afe8d
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-09 11:34:25 -0600

    add test for the food truck names method

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,4 +1,7 @@
 require './lib/event'
+require './lib/item'
+require './lib/food_truck'
+
 RSpec.describe Event do
 
   it 'exist' do
@@ -17,5 +20,34 @@ RSpec.describe Event do
     event = Event.new("South Pearl Street Farmers Market")
 
     expect(event.food_trucks).to eq([])
+  end
+
+  it 'can add food truck to an array' do
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    expect(event.food_trucks).to eq([])
+
+    event.add_food_truck(food_truck1)
+
+    expect(event.food_trucks.length).to eq(1)
+
+  end
+
+  it 'can display all food truck names' do
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    event.add_food_truck(food_truck1)
+
+    expect(event.food_truck_names).to eq("South Pearl Street Farmers Market")
   end
 end
--- spec/food_truck.rb
+++ spec/food_truck.rb
@@ -67,6 +67,7 @@ RSpec.describe FoodTruck do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
 
     food_truck.stock(item1, 30)
 

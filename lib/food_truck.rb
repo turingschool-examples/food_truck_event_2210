e48c814b5745d10d74a0f2b1ef1377475136b680
Drew Layton <dlayton66@gmail.com>
2022-11-09 09:19:34 -0700

    feat/test: Create FoodTruck class

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -2,5 +2,11 @@ require './lib/item'
 require './lib/food_truck'
 
 class FoodTruck
-
+  attr_reader :name,
+              :inventory
+              
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,9 +1,13 @@
-require './lib/foodtruck'
+require './lib/food_truck'
 
 describe FoodTruck do
-  before :all do
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-  end
+  let(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+  let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+  let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
 
+  it 'exists and has attributes' do
+    expect(food_truck).to be_a FoodTruck
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+    expect(food_truck.inventory).to eq({})
+  end
 end

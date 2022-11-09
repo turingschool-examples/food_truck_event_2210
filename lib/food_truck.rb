2b3323af45513e53b5aede8979f2c5e2354f9fbe
Drew Layton <dlayton66@gmail.com>
2022-11-09 09:32:40 -0700

    feat/test: Create #stock and #check_stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,9 +4,17 @@ require './lib/food_truck'
 class FoodTruck
   attr_reader :name,
               :inventory
-              
+
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item,quantity)
+    @inventory[item] += quantity
   end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require './lib/food_truck'
+# require './lib/item'
 
 describe FoodTruck do
   let(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
@@ -9,5 +10,34 @@ describe FoodTruck do
     expect(food_truck).to be_a FoodTruck
     expect(food_truck.name).to eq("Rocky Mountain Pies")
     expect(food_truck.inventory).to eq({})
+  end
+
+  describe '#check_stock' do
+    it 'returns the stock of an item, 0 by default' do
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+  end
+
+  describe '#stock' do
+    it 'sets the stock of an item to a value' do
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.check_stock(item1)).to eq(30)
+    end
+    
+    it 'adds that item to the inventory hash' do
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.inventory).to eq({item1 => 30})
+      expect(food_truck.check_stock(item1)).to eq(30)
+
+      food_truck.stock(item1, 25)
+
+      expect(food_truck.check_stock(item1)).to eq(55)
+
+      food_truck.stock(item2, 12)
+
+      expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
+    end
   end
 end

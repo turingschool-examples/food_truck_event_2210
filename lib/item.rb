a753f6d25a3871df9d674362b165cbef8f05e28f
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-09 09:10:57 -0700

    Created attr reader and initialize for item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,8 @@
 class Item
-
+  attr_reader :name,
+              :price
+  def initialize(hash)
+    @name = hash[:name]
+    @price = hash[:price]
+  end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe Item do
     it 'exists and has readable attributes' do
       expect(item1).to be_a(Item)
       expect(item2).to be_a(Item)
-      exect(item1.name).to eq('Peach Pie (Slice)')
-      exect(item2.name).to eq('Apple Pie (Slice)')
-      exect(item1.price).to eq('$3.75')
-      exect(item2.price).to eq('$2.50')
+      expect(item1.name).to eq('Peach Pie (Slice)')
+      expect(item2.name).to eq('Apple Pie (Slice)')
+      expect(item1.price).to eq('$3.75')
+      expect(item2.price).to eq('$2.50')
     end
   end
 end

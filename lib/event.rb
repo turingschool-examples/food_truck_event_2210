95fd5a4fff8204dff67b359c9551970bac4b6bd3
Samuel Cox <samc1253@gmail.com>
2022-11-09 09:56:10 -0700

    Add method to find food trucks that sell a specific item '

--- lib/event.rb
+++ lib/event.rb
@@ -12,4 +12,10 @@ class Event
   def food_truck_names
     food_trucks.map { |food_truck| food_truck.name}
   end
+
+  def food_trucks_that_sell(item)
+    food_trucks.find_all do |food_truck|
+      food_truck.check_stock(item) != 0
+    end
+  end
 end

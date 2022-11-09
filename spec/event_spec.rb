cd7fff5bcd73206396f57501cebeb6d7cadd8fe0
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-11-09 10:45:20 -0800

    Refactors spec to better test everything

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -86,6 +86,46 @@ RSpec.describe 'Event' do
     end
   end
 
+  describe '#food_truck_item_quantity(item)' do
+    it 'will return the total quantity across all food trucks' do
+      event = Event.new("South Pearl Street Farmers Market") 
+
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      food_truck1.add_item(item1)
+      food_truck1.add_item(item2)
+
+      food_truck1.stock(item1, 35) 
+      food_truck1.stock(item2, 7)  
+
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+
+      food_truck2.add_item(item3)
+      food_truck2.add_item(item4)
+
+      food_truck2.stock(item4, 50) 
+      food_truck2.stock(item3, 25)
+
+      food_truck3 = FoodTruck.new("Palisade Peach Shack") 
+
+      food_truck3.add_item(item1)
+
+      food_truck3.stock(item1, 65) 
+
+      event.add_food_truck(food_truck1) 
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.food_truck_item_quantity(item1)).to eq(100)
+      expect(event.food_truck_item_quantity(item2)).to eq(7)
+    end
+  end
+
   describe '#over_stocked_item?(item)' do
     it 'will return true or false whether the item is over stocked' do
       event = Event.new("South Pearl Street Farmers Market") 
@@ -168,5 +208,5 @@ RSpec.describe 'Event' do
     end
   end
 
-  
+
 end

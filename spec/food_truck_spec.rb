53c28a35b8db788879c98ff83d8ba2f84fe9a14f
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-09 10:16:58 -0600

    Add food truck check stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require './lib/food_truck'
 require './lib/item'
 
 describe FoodTruck do
@@ -8,7 +9,16 @@ describe FoodTruck do
 
       expect(food_truck).to be_a FoodTruck
       expect(food_truck.name).to eq 'Rocky Mountain Pies'
-      expect(food_truck.inventory).to eq {}
+      expect(food_truck.inventory).to eq({})
+    end
+  end
+
+  describe '#check_stock' do
+    it 'checks the quantity of an Item on the food truck' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+      expect(food_truck.check_stock(item1)).to eq 0
     end
   end
 end

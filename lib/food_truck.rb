6f22f705d17a1f18456bd2c50bf58194f90fa72f
Anhnhi Tran <anhtran811@gmail.com>
2022-11-09 09:38:23 -0700

    Completed iteration 1

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,25 @@
 class FoodTruck
+  attr_reader :name,
+              :inventory
 
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory[item].nil?
+      0
+    else
+      @inventory[item]
+    end
+  end
+
+  def stock(item, count)
+    if @inventory[item].nil?
+      @inventory[item] = count
+    else 
+      @inventory[item] += count
+    end
+  end
 end

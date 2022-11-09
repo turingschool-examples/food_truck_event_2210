44204a5fadbc3e11082927eb824ed08119ae5e34
Samuel Cox <samc1253@gmail.com>
2022-11-09 10:00:08 -0700

    Add test for finding overstocked items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -47,4 +47,12 @@ RSpec.describe Event do
     expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
   end
+
+  it 'can return the items that are overstocked' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expect(@event.overstocked_items).to eq([@food_truck1])
+  end
 end

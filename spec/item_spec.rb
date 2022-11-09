10ed106f6889cd40d30b33ab461fbd85118f100d
Joe King <j.king92@gmail.com>
2022-11-09 09:09:00 -0700

    test: that item has a name/price that can be read

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Item do
 
     expect(item).to be_a Item
   end
+
+  it 'has a name and a price' do
+    item = Item.new({name: 'Pumpkin Pie (Slice)', price: '$1.99'})
+
+    expect(item.name).to eq('Pumpkin Pie (Slice)')
+    expect(item.price).to eq('$1.99')
+  end
 end

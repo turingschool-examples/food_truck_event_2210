6c01b1016d61cdc193422cebf5c557db0323921d
Kelsie Porter <krporter22@gmail.com>
2022-11-09 10:06:15 -0700

    Create #add_food_trucks tests

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,14 @@
 class Event
+  attr_reader :name,
+              :food_trucks
+
+  def initialize(name)
+    @name = name
+    @food_trucks = []    
+  end
+
+  def food_truck_names
+    # iterate over the food trucks and return an array of their names
+  end
 
 end

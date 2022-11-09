9515954801e743c9a486770620586a60d7483bc5
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-09 10:34:40 -0600

    Have a start on the food truck class

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,25 @@
 class FoodTruck
 
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    # require 'pry'; binding.pry
+    if inventory.keys.any? do |key|
+        key = item
+        puts key.value
+      end
+    else
+      0
+    end
+  end
+
+  def stock(item, amount)
+    amount = inventory[:item]
+  end
 end

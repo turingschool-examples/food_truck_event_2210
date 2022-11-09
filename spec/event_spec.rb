8e97cee8b33ec0f74b1450624fd169801409ee1e
Ryan Canton <ryancanton999@gmail.com>
2022-11-09 09:39:47 -0700

    Test: Add test for food_truck_names method for class Event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -26,4 +26,18 @@ RSpec.describe Event do
       expect(event.food_trucks[0]).to be_a(FoodTruck)
     end
   end
+
+  describe '#food_truck_names' do
+    it 'returns an array of strings representing food truck names' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
 end

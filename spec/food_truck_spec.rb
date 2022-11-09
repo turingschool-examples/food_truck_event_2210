a0b941b984623176e45f01eaa54c303c6f2da96a
Joe King <j.king92@gmail.com>
2022-11-09 09:21:47 -0700

    test: #stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,6 +3,9 @@ require './lib/food_truck'
 
 RSpec.describe FoodTruck do
   let(:ft) {FoodTruck.new('Rock Springs Cafe')}
+  let(:it1) {Item.new({name: 'Pumpkin Pie (Slice)', price: '$1.99'})}
+  let(:it2) {Item.new({name: 'Rhubarb Pie (Slice)', price: '$0.99'})}
+
   it 'exists' do
     expect(ft).to be_a FoodTruck
   end
@@ -14,6 +17,14 @@ RSpec.describe FoodTruck do
   describe '#inventory' do
     it 'starts as an empty hash' do
       expect(ft.inventory).to eq({})
+    end
+  end
+
+  describe '#stock()' do
+    it 'adds an item and quantity of item to hash' do
+      ft.stock(it1, 20)
+
+      expect(ft.inventory).to eq({it1 => 20})
     end
   end
 end

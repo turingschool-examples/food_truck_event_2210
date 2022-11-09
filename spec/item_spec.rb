89f1b9ad3c28dc0fa5b0500426d2ae32f77c96a2
KaraJH <karas.jones@yahoo.com>
2022-11-09 09:20:46 -0700

    Tests for item object and attributes

---
+++ spec/item_spec.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+
+RSpec.describe Item do
+  it 'exists and has attributes' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    expect(item1).to be_a(Item)
+    expect(item1.name).to eq('Peach Pie (Slice)')
+    expect(item1.price).to eq("$3.75")
+  end
+end

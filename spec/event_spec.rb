f248e4a11d68aef327105a3d63cfaa43f2fc5582
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-09 12:37:34 -0600

    Add event total_inventory test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -196,4 +196,41 @@ describe Event do
       expect(event.all_event_item_names).to eq ['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream']
     end
   end
+
+  describe '#total_inventory' do
+    it 'returns a hash of all items sold at event with their quantity and food trucks' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      food_truck1.stock(item1, 35)    
+      food_truck1.stock(item2, 7)    
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")    
+      food_truck2.stock(item4, 51)    
+      food_truck2.stock(item3, 25)
+      food_truck2.stock(item2, 25)
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")    
+      food_truck3.stock(item1, 65)  
+
+      event.add_food_truck(food_truck1)    
+      event.add_food_truck(food_truck2)    
+      event.add_food_truck(food_truck3)
+
+      hash = event.total_inventory
+      items = event.total_inventory.keys
+      item_info = event.total_inventory.values
+      hash_item1 = event.total_inventory.keys.first
+      hash_item1_quantity = event.total_inventory[item1][quantity]
+      hash_item1_foodtrucks = event.total_inventory[item1][sold_at]
+
+      expect(hash.class).to eq Hash
+      expect(items.all?(Item)).to be true
+      expect(item_info.class).to eq Hash
+      expect(hash_item1).to eq item1
+      expect(hash_item1_quantity).to eq 100
+      expect(hash_item1_foodtrucks).to eq event.food_trucks_that_sell(item1)
+    end
+  end
 end

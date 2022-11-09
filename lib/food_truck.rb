4794d82cb1492c2d03995ad958230721125f4116
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-09 09:43:27 -0700

    Feat: check_stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,9 +1,22 @@
+require 'pry'
+
 class FoodTruck
     attr_reader :name,
                 :inventory
   def initialize (name)
     @name = name
     @inventory = {}
+  end
+
+#   def stock(item, qty = 0)
+#     @inventory[item]
+#     binding.pry
+#   end
+
+  def check_stock(item)
+    if !@inventory.include?(item)
+    return 0
+    end
   end
 
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require './lib/food_truck'
+require './lib/item'
 require 'pry'
 
 RSpec.describe FoodTruck do
@@ -9,6 +10,14 @@ RSpec.describe FoodTruck do
     expect(food_truck.name).to eq("Rocky Mountain Pies")
     expect(food_truck.inventory).to eq({})
   end
-
   
+  it '#stock is 0 by default' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3:75"})
+    item2 = Item.new({name: "Apple Pie (Slice)", price: "$2.50"})
+
+    expect(food_truck.check_stock(item1)).to eq(0) 
+    expect(food_truck.check_stock(item2)).to eq(0) 
+  end
+
 end

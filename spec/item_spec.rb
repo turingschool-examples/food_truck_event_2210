e45fef1bd4ecf377d0a13da3d9b609b73080cd2e
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-09 09:07:26 -0700

    Feat: add initialize method for class Item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Item do
       item1 = Item.new({name: 'Peach pie (slice)', price: "$3.75"})
 
       expect(item1).to be_a(Item)
-      expect(item1.name).to eq('Peach pic (slice')
+      expect(item1.name).to eq('Peach pie (slice)')
       expect(item1.price).to eq("$3.75")
     end 
   end 

f2f0f51397a9ec2017f23e46649d5150cb2a1a95
Alastair Lloyd <dex8056@aol.com>
2022-11-09 09:54:55 -0700

    Add a seperate stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -11,12 +11,25 @@ RSpec.describe do
     expect(food_truck.check_stock(item1)).to eq 0
   end
 
-  it 'can add stock' do
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+  describe '#check_stock' do
+    it 'returns stock level' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 
-    expect(food_truck.check_stock(item1)).to eq 0
-    food_truck.stock(item1, 30)
-    expect(food_truck.check_stock(item1)).to eq 30
+      expect(food_truck.check_stock(item1)).to eq 0
+      food_truck.stock(item1, 30)
+      expect(food_truck.check_stock(item1)).to eq 30
+    end
+  end
+
+  describe '#stock' do
+    it 'can add stock to inventory' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+      expect(food_truck.check_stock(item1)).to eq 0
+      food_truck.stock(item1, 30)
+      expect(food_truck.check_stock(item1)).to eq 30
+    end
   end
 end

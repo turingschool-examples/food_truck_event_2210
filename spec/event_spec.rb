4c98e98c2ed158ccdc118072beb439c92e86dd14
Ryan Canton <ryancanton999@gmail.com>
2022-11-09 09:29:31 -0700

    Test: Add tes fir initialize method for class Event

---
+++ spec/event_spec.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+Rspec.describe Event do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event.name).to eq("South Pearl Street Farmers Market")
+      expect(event.food_trucks).to eq([])
+    end
+  end
+end

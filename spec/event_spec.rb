d65e113d4390b4b1a1fe1816a08824b96cde7a99
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-09 08:47:08 -0800

    Test: add food truck

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -2,13 +2,30 @@ require './lib/event'
 
 RSpec.describe Event do
   it 'exists' do
-    event = Event.new("South Pearl Street Farmers Market")
+    event = Event.new('South Pearl Street Farmers Market')
     expect(event).to be_instance_of(Event)
-    expect(event.name).to eq("South Pearl Farmers Market")
+    expect(event.name).to eq('South Pearl Street Farmers Market')
     expect(event.food_trucks).to eq([])
   end
 
   it '#add_food_truck to event' do
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+    item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
+    item3 = Item.new({ name: 'Peach-Raspberry Nice Cream', price: '$5.30' })
+    item4 = Item.new({ name: 'Banana Nice Cream', price: '$4.25' })
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.food_trucks.length).to eq(3)
+    expect(event.food_trucks[0]).to be_instance_of(FoodTruck) 
   end
 
   it '#food_truck_names returns list of names for event' do
@@ -17,7 +34,7 @@ RSpec.describe Event do
   it '#food_trucks_that_sell Return the list of FoodTrucks that sell a given Item for an Event' do
   end
 
-  it '#potential_revenue from a FoodTruck' do
+  xit '#potential_revenue from a FoodTruck' do
     expect(event.food_truck_names).to be_a(Array)
   end
 end

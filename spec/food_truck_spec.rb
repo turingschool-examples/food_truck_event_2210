e06760d6c87f02406092d51368f4d9803a831422
J Seymour <JustJakeSeymour@gmail.com>
2022-11-09 09:11:59 -0700

    write tests for food truck class iteration 1

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,38 @@
+require './lib/food_truck'
+require './lib/item'
+
+RSpec.describe FoodTruck do
+  let!(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+  let!(:item_1) {Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})}
+  let!(:item_2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+
+  it 'starts with a name and empty inventory hash' do
+    expect(foot_truck.name).to eq("Rocky Mountain Pies")
+    expect(foot_truck.inventory).to eq({})
+    expect(foot_truck).to be_a(FoodTruck)
+  end
+  
+  it 'can have items added with a stock method' do
+    expect(food_truck.check_stock(item_1)).to eq(0)
+    
+    food_truck.stock(item1, 30)
+    
+    expect(food_truck.inventory).to eq({ item_1: 30 })
+    expect(food_truck.check_stock(item_1)).to eq(30)
+  end
+  
+  it 'adds more stock if same item is added' do
+    food_truck.stock(item1, 30)
+    
+    expect(food_truck.inventory).to eq({ item_1: 30 })
+    
+    food_truck.stock(item1, 25)
+    
+    expect(food_truck.inventory).to eq({ item_1: 55 })
+    
+    food_truck.stock(item2, 12)
+
+    expect(food_truck.inventory).to eq({ item_1: 55, item_2: 12 })
+  end
+  
+end

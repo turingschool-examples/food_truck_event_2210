eeb363158a331daca1d47abdfe0ee530de3c2d24
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-09 11:49:20 -0600

    Have the food trucks that sell method passing

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,26 @@
 class Event
 
+  attr_reader :name,
+              :food_trucks
+
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(truck)
+    food_trucks.push(truck)
+  end
+
+  def food_truck_names
+    food_trucks.map do |truck|
+      truck.name
+    end
+  end
+
+  def food_trucks_that_sell(item)
+    food_trucks.find_all do |trucks|
+      trucks.inventory[item] > 0
+    end
+  end
 end

3f108c6bf25ca26aaf96bff989622394eb9a805d
Aether Kerstens <philippekerstens@gmail.com>
2022-11-09 09:59:14 -0800

    Refactor tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -34,11 +34,11 @@ RSpec.describe FoodTruck do
 
   it 'can stock item' do
     @food_truck.stock(@item1, 30)
-    expect(@food_truck.inventory).to eq({@item1})
+    expect(@food_truck.inventory).to eq({@item1 => 30})
     expect(@food_truck.check_stock(@item1)).to eq(30)
     @food_truck.stock(@item1, 25)
     expect(@food_truck.check_stock(@item1)).to eq(55)
     @food_truck.stock(@item2, 12)
-    expect(@food_truck.inventory).to eq({@item1, @item2})
+    expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
   end
 end
